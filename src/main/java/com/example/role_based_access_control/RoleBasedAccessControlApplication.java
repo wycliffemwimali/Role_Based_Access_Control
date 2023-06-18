f6dd53a0f1201c36583207f358f4b42f59137fdc
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-18 19:20:13 +0300

    files updated

---
+++ src/main/java/com/example/role_based_access_control/RoleBasedAccessControlApplication.java
@@ -0,0 +1,13 @@
+package com.example.role_based_access_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAccessControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleBasedAccessControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/role_based_access_control/RoleBasedAccessControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.role_based_access_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAccessControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
