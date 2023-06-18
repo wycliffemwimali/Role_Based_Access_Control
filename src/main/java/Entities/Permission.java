ce6c7e28aecc9139257f4cf2f21f79c1312b790d
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-18 20:26:27 +0300

    attributes

--- src/main/java/Entities/Permission.java
+++ src/main/java/Entities/Permission.java
@@ -1,4 +1,15 @@
 package Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
