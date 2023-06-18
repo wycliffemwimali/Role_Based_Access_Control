5b52fb55fcff9f1bf84331881a9a0bb32ed5a3e2
wycliffemwimali <wycliffemwimali2@gmail.com>
2023-06-18 20:45:02 +0300

    PermissionRepo

---
+++ src/main/java/Repository/PermissionRepository.java
@@ -0,0 +1,8 @@
+package Repository;
+
+import Entities.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Permission findByName(String name);
+}
